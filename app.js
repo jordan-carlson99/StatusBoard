d6a546b997ed3e2ecb7acef2f6c2a8d89891f827
jordan-carlson99 <116410897+jordan-carlson99@users.noreply.github.com>
2023-02-19 17:27:58 -0600

    Add files via upload

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-class AvEditor {
+class AV {
     constructor (adminNumber, hours, status, twentyFive, fifty, oneTwentyFive, twoFifty, fiveHundred, miniTUp, miniTDown, miniCUp, uhf) {
         this.adminNumber = adminNumber || null
         this.hours = hours || 0
@@ -12,69 +12,75 @@ class AvEditor {
         this.miniTUp = miniTUp || '00.000'
         this. miniCUp = miniCUp || '00.000'
         this.uhf = uhf || '000.0000'
+    }
 
-        // an array of all column classes and constructor vars to iterate through, eventually normalize all the names to one (ie 25 hours is twentyFive)
-        let arrayOfColumnClasses = 
-        ['hours','status','25 hours','50 hours','125 hours','250 hours',
-        '500 hours','Mini-T up','Mini-T down','Mini-C up','UHF']
-        let arrayOfVars = 
-        ['hours', 'status', 'twentyFive', 'fifty', 'oneTwentyFive',
-        'twoFifty', 'fiveHundred', 'miniTDown', 'miniTUp', 'miniCUp', 'uhf']
-
-        if (!document.getElementById(this.adminNumber)){
-            //only create if admin number doesnt exist
-            let newItem = document.createElement("th")
-            newItem.innerText = this.adminNumber
-            newItem.id = this.adminNumber
-            document.getElementsByClassName('adminNumber')[0].appendChild(newItem)
-            for (var i = 0; i < arrayOfColumnClasses.length; i++) {
-                this.createNewRow(arrayOfColumnClasses[i], this.adminNumber, this[arrayOfVars[i]])
-            }
-        } else if (document.getElementById(this.adminNumber)){
-            for (var i = 0; i < arrayOfColumnClasses.length; i++) {
-                this.appendExistingRow(arrayOfColumnClasses[i], this.adminNumber, this[arrayOfVars[i]])
-            }
+    read () {
+        let adminNo = this.adminNumber 
+        if (document.getElementById(adminNo)) {
+            // send given object (arrayOfObj[i] to append to check if any data changed
+            this.append()
+        } else {
+            this.create()
+            // send to create to make new table columns
         }
     }
 
-    createNewRow (columnClass, adminNumber, newValue) {
-        let newObj = document.createElement('td')
-        newObj.id = `${adminNumber} ${columnClass}`
-        newObj.innerText = newValue
-        document.getElementsByClassName(columnClass)[0].appendChild(newObj)
-    }
-    appendExistingRow (columnClass, adminNumber, newValue) {
-        document.getElementById(`${adminNumber} ${columnClass}`).textContent = newValue
-    }
-    removeAv (adminNumber) {
-        for (var i = 0; i < arrayOfColumnClasses.length; i++) {
-            if (document.getElementById(`${adminNumber} ${arrayOfColumnClasses[i]}`)) {
-                document.getElementById(`${adminNumber} ${arrayOfColumnClasses[i]}`).remove()
-            } else {
-                alert(`AV ${adminNumber} Removed`)
-            }
+    create () {
+        // create new row with tailnumber
+        let newColumn = document.createElement("th")
+        newColumn.innerText = this.adminNumber
+        newColumn.id = this.adminNumber
+        document.getElementById('adminNumber').appendChild(newColumn)
+
+        // add in each piece of data
+        for (var i = 0; i < arrayOfVars.length; i++) {
+            let newData = document.createElement('td')
+            newData.id = `${this.adminNumber} ${arrayOfVars[i]}`
+            newData.innerText = this[arrayOfVars[i]]
+            document.getElementById(arrayOfVars[i]) = newData
         }
     }
+
+    append() {
+        for (var i = 0; i < arrayOfVars.length; i++) {
+            document.getElementById(`${this.adminNumber} ${arrayOfVars[i]}`).textContent = `${this[arrayOfVars[i]]} ${arrayOfVars[i]}`
+        }
+    }
+
+    // removeAv (adminNumber) {
+    //     for (var i = 0; i < arrayOfColumnClasses.length; i++) {
+    //         if (document.getElementById(`${adminNumber} ${arrayOfVars[i]}`)) {
+    //             document.getElementById(`${adminNumber} ${arrayOfVars[i]}`).remove()
+    //         } else {
+    //             alert(`AV ${adminNumber} Removed`)
+    //         }
+    //     }
+    // }
     // method for editing
     // method for adding hours
 }
 
+let arrayOfVars = ['hours', 'status', 'twentyFive', 'fifty', 'oneTwentyFive',
+'twoFifty', 'fiveHundred', 'miniTDown', 'miniTUp', 'miniCUp', 'uhf']
+
 let avList = []
 
 function avSubmit () {
-    let adminNumber = document.getElementById('adminNumber').value
-    let status = document.getElementById('status').value
-    let hours = document.getElementById('hours').value
-    let twentyFiveHours = document.getElementById('25 hours').value
-    let fiftyHours = document.getElementById('50 hours').value
-    let oneTwentyFiveHours = document.getElementById('125 hours').value
-    let twoFiftyHours = document.getElementById('250 hours').value
-    let fiveHundredHours = document.getElementById('500 hours').value
-    let miniTDown = document.getElementById('Mini-T down').value
-    let miniTUp = document.getElementById('Mini-T up').value
-    let miniCUp = document.getElementById('Mini-C up').value
-    let uhf = document.getElementById('UHF').value
-    const AV = new AvEditor (adminNumber, hours, status, twentyFiveHours, fiftyHours, oneTwentyFiveHours, twoFiftyHours, fiveHundredHours,miniTUp, miniTDown, miniCUp, uhf)
-    avList.push(AV)
+    let adminNumber = document.getElementById('adminNumberInput').value
+    let status = document.getElementById('statusInput').value
+    let hours = document.getElementById('hoursInput').value
+    let twentyFive = document.getElementById('twentyFiveInput').value
+    let fifty = document.getElementById('fiftyInput').value
+    let oneTwentyFive = document.getElementById('oneTwentyFiveInput').value
+    let twoFifty = document.getElementById('twoFiftyInput').value
+    let fiveHundred = document.getElementById('fiveHundredInput').value
+    let miniTDown = document.getElementById('miniTDownInput').value
+    let miniTUp = document.getElementById('miniTUpInput').value
+    let miniCUp = document.getElementById('miniCUpInput').value
+    let uhf = document.getElementById('uhfInput').value
+    const av = new AV (adminNumber, hours, status, twentyFive, fifty, oneTwentyFive, twoFifty, fiveHundred,miniTUp, miniTDown, miniCUp, uhf)
+    av.read()
+    avList.push(av)
     console.log(avList)
 }
+
