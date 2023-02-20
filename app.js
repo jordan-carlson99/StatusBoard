825fe8b39c273e0ecaea68fe4db06902298fe144
jordan-carlson99 <116410897+jordan-carlson99@users.noreply.github.com>
2023-02-20 16:07:44 -0600

    Drop down, editing data, removals, launchers
    Added drop down for adding and editing AVs so the user can just select what av they need to edit. 
    Data editing now takes place via DOM created text inputs instead of hard coded boxes. 
    Removing AVs is a separate field so there wouldn't be accidental deletions. 
    Began on launchers.
    
    Need to work through user edge cases like accidentally creating an AV with no text (currently handled by constructor default of ' '), The CSS is abysmal and needs polishing too. 
    I need to separate equipment into different modules I could import (never done that before) so I can keep data bleed over to a minimum. 
    Need to start adding pages, one for a display of all equipment in one shot.
    Finally there needs to be persistent data with backups.

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 class AV {
     constructor (adminNumber, hours, status, twentyFive, fifty, oneTwentyFive, twoFifty, fiveHundred, miniTUp, miniTDown, miniCUp, uhf, notes) {
-        this.adminNumber = adminNumber || null
+        this.adminNumber = adminNumber ||  ' '
         this.hours = hours || 0
         this.status = status || 'X'
         this.twentyFive = twentyFive || 25
@@ -17,6 +17,7 @@ class AV {
 
     read () {
         let adminNo = this.adminNumber 
+        if (document.getElementById)
         if (document.getElementById(adminNo)) {
             this.append()
         } else {
@@ -25,23 +26,38 @@ class AV {
     }
 
     create () {
-        // create new row with tailnumber
         let newColumn = document.createElement('th');
         newColumn.innerText = this.adminNumber;
         newColumn.id = this.adminNumber;
         document.getElementById('adminNumber').appendChild(newColumn);
+
         for (var i = 0; i < arrayOfVars.length; i++) {
             let newData = document.createElement('td');
             newData.id = `${this.adminNumber} ${arrayOfVars[i]}`;
             newData.innerText = this[arrayOfVars[i]];
+            newData.className = `avDataCell`
             document.getElementById(arrayOfVars[i]).appendChild(newData);
         }
+        let newMenuItem = document.createElement('a')
+        newMenuItem.className = 'button'
+        newMenuItem.onclick = function () {inputMenu(true, this.innerText)}
+        newMenuItem.id = `${this.adminNumber} Menu Item`
+        newMenuItem.textContent = this.adminNumber
+        document.getElementById('avDropDown').appendChild(newMenuItem)
+        avList.push(this)
     }
 
     append() {
-        for (var i = 0; i < arrayOfVars.length; i++) {
-            document.getElementById(`${this.adminNumber} ${arrayOfVars[i]}`).textContent = `${this[arrayOfVars[i]]}`
-            // remove old objects from av list
+        let tailNo = this.adminNumber
+        for(var i = 0; i < avList.length; i++) {
+            if (avList[i].adminNumber === tailNo) {
+                let oldAv = avList[i]
+                for (var i = 0; i < arrayOfVars.length; i++) {
+                    let updated = this[arrayOfVars[i]]
+                    document.getElementById(`${tailNo} ${arrayOfVars[i]}`).textContent = `${updated}`
+                    oldAv[arrayOfVars[i]] = updated
+                }
+            }
         }
     }
 
@@ -57,10 +73,6 @@ class AV {
         }
     }
 
-    avEdit () {
-        //method to edit values using html dropdown bar
-    }
-
     avAddHours () {
         // method for adding total hours so 25,50,125,250,and 500 update
     }
@@ -71,8 +83,8 @@ let arrayOfVars = ['hours', 'status', 'twentyFive', 'fifty', 'oneTwentyFive',
 
 let avList = []
 
-function avSubmit () {
-    let adminNumber = document.getElementById('adminNumberInput').value
+function avCreate () {
+    let adminNumber = document.getElementById('adminNumberInput').value 
     let status = document.getElementById('statusInput').value
     let hours = document.getElementById('hoursInput').value
     let twentyFive = document.getElementById('twentyFiveInput').value
@@ -86,13 +98,14 @@ function avSubmit () {
     let uhf = document.getElementById('uhfInput').value
     let notes = document.getElementById('avNotes').value
     const av = new AV (adminNumber, hours, status, twentyFive, fifty, oneTwentyFive, twoFifty, fiveHundred,miniTUp, miniTDown, miniCUp, uhf, notes)
+    document.getElementById('avInputs').innerHTML = ''
     av.read()
-    avList.push(av)
+
     console.log(avList)
 }
 
 function removeAv () {
-    let adminNumber = document.getElementById('adminNumberInput')
+    let adminNumber = document.getElementById('avRemoveInput')
     for (var i = 0; i < avList.length; i++) {
         if (avList[i].adminNumber === adminNumber.value) {
             let result = confirm(`Remove ${avList[i].adminNumber}?`)
@@ -104,9 +117,183 @@ function removeAv () {
     }
 }
 
+function avEdit () {
+    let cells = document.getElementsByClassName('avDataCell')
+    for (var i = 0; i < cells.length; i++) {
+        let cell = cells[i]
+        let inputCell = document.createElement('input')
+        cell.replaceWith(inputCell)
+    }
+}
+
 var intervalId = window.setInterval(function(){
     for(var i = 0; i < avList.length; i++) {
         avList[i].read()
         document.getElementById('update').textContent = `Updated ${Date()}`
     }
   }, 60000);
+
+  function inputMenu(bool, tailNo) {
+    let inputTable = document.getElementById('avInputs')
+    let tailNumber = document.createElement('input')
+    tailNumber.type = 'text'
+    tailNumber.id = `adminNumberInput`
+    if (bool === false) {
+        tailNumber.placeholder = 'Admin Number'
+        inputTable.appendChild(tailNumber)
+
+        for (var i = 0; i < arrayOfVars.length; i++) {
+            let inputBox = document.createElement('input')
+            inputBox.id = `${arrayOfVars[i]}Input`
+            inputBox.type = 'text'
+            inputBox.placeholder = arrayOfVars[i]
+            inputTable.appendChild(inputBox)
+        }
+    } else if(bool = true){
+        for (var i = 0; i < avList.length; i++) {
+            if (avList[i].adminNumber === tailNo) {
+                var currentData = avList[i]
+                break
+            }
+        }
+        tailNumber.value = tailNo
+        inputTable.appendChild(tailNumber)
+        for (var i = 0; i < arrayOfVars.length; i++) {
+            let inputBox = document.createElement('input')
+            inputBox.id = `${arrayOfVars[i]}Input`
+            inputBox.type = 'text'
+            inputBox.value = currentData[arrayOfVars[i]]
+            inputTable.appendChild(inputBox)
+        }
+    }
+    let submitButton = document.createElement('button')
+    submitButton.type = 'button'
+    submitButton.onclick = function() {avCreate()}
+    submitButton.textContent = 'Submit'
+    inputTable.appendChild(submitButton)
+  }
+
+
+// LAUNCHERS ------------------------------------------------------------------------------------------------------------
+
+
+  class Lau {
+    constructor (adminNumber, status, launches, notes) {
+        this.adminNumber = adminNumber ||  ' '
+        this.launches = launches || 0
+        this.status = status || 'X'
+        this.notes = notes || ''
+    }
+
+    lauRead () {
+        let adminNo = this.adminNumber 
+        if (document.getElementById)
+        if (document.getElementById(adminNo)) {
+            this.lauAppend()
+        } else {
+            this.lauCreate()
+        }
+    }
+
+    lauCreate () {
+        let newColumn = document.createElement('th');
+        newColumn.innerText = this.adminNumber;
+        newColumn.id = this.adminNumber;
+        document.getElementById('lauAdminNumber').appendChild(newColumn);
+
+        for (var i = 0; i < lauArrayOfVars.length; i++) {
+            let newData = document.createElement('td');
+            newData.id = `${this.adminNumber} ${lauArrayOfVars[i]}`;
+            newData.innerText = this[lauArrayOfVars[i]];
+            newData.className = `lauDataCell`
+            document.getElementById(`lau${lauArrayOfVars[i]}`).appendChild(newData);
+        }
+        let newMenuItem = document.createElement('a')
+        newMenuItem.className = 'button'
+        newMenuItem.onclick = function () {inputMenu(true, this.innerText)}
+        newMenuItem.id = `${this.adminNumber} Menu Item`
+        newMenuItem.textContent = this.adminNumber
+        document.getElementById('lauDropDown').appendChild(newMenuItem)
+        lauList.push(this)
+    }
+
+    lauAppend() {
+        let lauNo = this.adminNumber
+        for(var i = 0; i < lauList.length; i++) {
+            if (lauList[i].adminNumber === lauNo) {
+                let oldLau = avList[i]
+                for (var i = 0; i < arrayOfVars.length; i++) {
+                    let updated = this[lauArrayOfVars[i]]
+                    document.getElementById(`${lauNo} ${lauArrayOfVars[i]}`).textContent = `${updated}`
+                    oldLau[lauArrayOfVars[i]] = updated
+                }
+            }
+        }
+    }
+
+    lauRemove () {
+        if (document.getElementById(this.adminNumber)) {
+            document.getElementById(this.adminNumber).remove()
+            for (var i = 0; i < lauArrayOfVars.length; i++) {
+                document.getElementById(`${this.adminNumber} ${lauArrayOfVars[i]}`).remove()
+            }
+            alert(`AV ${this.adminNumber} Removed`)
+        } else {
+            alert(`AV ${this.adminNumber} doesnt exist!`)
+        }
+    }
+}
+
+let lauArrayOfVars = ['launches', 'status']
+lauList = []
+
+function lauCreate () {
+    let adminNumber = document.getElementById('lauAdminNumberInput').value 
+    let status = document.getElementById('laustatusInput').value
+    let launches = document.getElementById('laulaunchesInput').value
+    let notes = document.getElementById('lauNotes').value
+    const lau = new Lau (adminNumber, status, launches, notes)
+    document.getElementById('avInputs').innerHTML = ''
+    lau.lauRead()
+    console.log(lauList)
+}
+
+function lauInputMenu(bool, lauNo) {
+    let inputTable = document.getElementById('lauInputs')
+    let tailNumber = document.createElement('input')
+    tailNumber.type = 'text'
+    tailNumber.id = `lauAdminNumberInput`
+    if (bool === false) {
+        tailNumber.placeholder = 'Admin Number'
+        inputTable.appendChild(tailNumber)
+
+        for (var i = 0; i < lauArrayOfVars.length; i++) {
+            let inputBox = document.createElement('input')
+            inputBox.id = `lau${lauArrayOfVars[i]}Input`
+            inputBox.type = 'text'
+            inputBox.placeholder = lauArrayOfVars[i]
+            inputTable.appendChild(inputBox)
+        }
+    } else if(bool = true){
+        for (var i = 0; i < avList.length; i++) {
+            if (avList[i].adminNumber === lauNo) {
+                var currentData = avList[i]
+                break
+            }
+        }
+        tailNumber.value = lauNo
+        inputTable.appendChild(tailNumber)
+        for (var i = 0; i < lauArrayOfVars.length; i++) {
+            let inputBox = document.createElement('input')
+            inputBox.id = `${lauArrayOfVars[i]}Input`
+            inputBox.type = 'text'
+            inputBox.value = currentData[lauArrayOfVars[i]]
+            inputTable.appendChild(inputBox)
+        }
+    }
+    let submitButton = document.createElement('button')
+    submitButton.type = 'button'
+    submitButton.onclick = function() {lauCreate()}
+    submitButton.textContent = 'Submit'
+    inputTable.appendChild(submitButton)
+  }
