a2d8e652aa5459dd23caa9b77a2c719727b1f4ae
jordan-carlson99 <116410897+jordan-carlson99@users.noreply.github.com>
2023-02-19 19:10:58 -0600

    Hashtable reading and notes
    Changed constructor to only behave like it should and modified the methods so they handled the HTML interfacing. cleaned up HTML, added update function and added notes section. 
    
    Tomorrow's task will be to create the edit method, HTML drop down bar, modify append method so it deletes the previous object, and write it all to a persistent text file

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 class AV {
-    constructor (adminNumber, hours, status, twentyFive, fifty, oneTwentyFive, twoFifty, fiveHundred, miniTUp, miniTDown, miniCUp, uhf) {
+    constructor (adminNumber, hours, status, twentyFive, fifty, oneTwentyFive, twoFifty, fiveHundred, miniTUp, miniTDown, miniCUp, uhf, notes) {
         this.adminNumber = adminNumber || null
         this.hours = hours || 0
         this.status = status || 'X'
@@ -12,52 +12,58 @@ class AV {
         this.miniTUp = miniTUp || '00.000'
         this. miniCUp = miniCUp || '00.000'
         this.uhf = uhf || '000.0000'
+        this.notes = notes || ''
     }
 
     read () {
         let adminNo = this.adminNumber 
         if (document.getElementById(adminNo)) {
-            // send given object (arrayOfObj[i] to append to check if any data changed
             this.append()
         } else {
             this.create()
-            // send to create to make new table columns
         }
     }
 
     create () {
         // create new row with tailnumber
-        let newColumn = document.createElement("th")
-        newColumn.innerText = this.adminNumber
-        newColumn.id = this.adminNumber
-        document.getElementById('adminNumber').appendChild(newColumn)
-
-        // add in each piece of data
+        let newColumn = document.createElement('th');
+        newColumn.innerText = this.adminNumber;
+        newColumn.id = this.adminNumber;
+        document.getElementById('adminNumber').appendChild(newColumn);
         for (var i = 0; i < arrayOfVars.length; i++) {
-            let newData = document.createElement('td')
-            newData.id = `${this.adminNumber} ${arrayOfVars[i]}`
-            newData.innerText = this[arrayOfVars[i]]
-            document.getElementById(arrayOfVars[i]) = newData
+            let newData = document.createElement('td');
+            newData.id = `${this.adminNumber} ${arrayOfVars[i]}`;
+            newData.innerText = this[arrayOfVars[i]];
+            document.getElementById(arrayOfVars[i]).appendChild(newData);
         }
     }
 
     append() {
         for (var i = 0; i < arrayOfVars.length; i++) {
-            document.getElementById(`${this.adminNumber} ${arrayOfVars[i]}`).textContent = `${this[arrayOfVars[i]]} ${arrayOfVars[i]}`
+            document.getElementById(`${this.adminNumber} ${arrayOfVars[i]}`).textContent = `${this[arrayOfVars[i]]}`
+            // remove old objects from av list
         }
     }
 
-    // removeAv (adminNumber) {
-    //     for (var i = 0; i < arrayOfColumnClasses.length; i++) {
-    //         if (document.getElementById(`${adminNumber} ${arrayOfVars[i]}`)) {
-    //             document.getElementById(`${adminNumber} ${arrayOfVars[i]}`).remove()
-    //         } else {
-    //             alert(`AV ${adminNumber} Removed`)
-    //         }
-    //     }
-    // }
-    // method for editing
-    // method for adding hours
+    avRemove () {
+        if (document.getElementById(this.adminNumber)) {
+            document.getElementById(this.adminNumber).remove()
+            for (var i = 0; i < arrayOfVars.length; i++) {
+                document.getElementById(`${this.adminNumber} ${arrayOfVars[i]}`).remove()
+            }
+            alert(`AV ${this.adminNumber} Removed`)
+        } else {
+            alert(`AV ${this.adminNumber} doesnt exist!`)
+        }
+    }
+
+    avEdit () {
+        //method to edit values using html dropdown bar
+    }
+
+    avAddHours () {
+        // method for adding total hours so 25,50,125,250,and 500 update
+    }
 }
 
 let arrayOfVars = ['hours', 'status', 'twentyFive', 'fifty', 'oneTwentyFive',
@@ -78,9 +84,29 @@ function avSubmit () {
     let miniTUp = document.getElementById('miniTUpInput').value
     let miniCUp = document.getElementById('miniCUpInput').value
     let uhf = document.getElementById('uhfInput').value
-    const av = new AV (adminNumber, hours, status, twentyFive, fifty, oneTwentyFive, twoFifty, fiveHundred,miniTUp, miniTDown, miniCUp, uhf)
+    let notes = document.getElementById('avNotes').value
+    const av = new AV (adminNumber, hours, status, twentyFive, fifty, oneTwentyFive, twoFifty, fiveHundred,miniTUp, miniTDown, miniCUp, uhf, notes)
     av.read()
     avList.push(av)
     console.log(avList)
 }
 
+function removeAv () {
+    let adminNumber = document.getElementById('adminNumberInput')
+    for (var i = 0; i < avList.length; i++) {
+        if (avList[i].adminNumber === adminNumber.value) {
+            let result = confirm(`Remove ${avList[i].adminNumber}?`)
+            if (result) {
+                avList[i].avRemove()
+                avList.splice(i,1)
+            }
+        }
+    }
+}
+
+var intervalId = window.setInterval(function(){
+    for(var i = 0; i < avList.length; i++) {
+        avList[i].read()
+        document.getElementById('update').textContent = `Updated ${Date()}`
+    }
+  }, 60000);
