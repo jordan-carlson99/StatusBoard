a1c84b79c2637f2c6395b63a494767bfb61e41d7
jordan-carlson99 <jordan.carlson.square@gmail.com>
2023-02-24 22:39:10 -0600

    CSS, Git, and Vscode prettier
    Removed in-line styles and added css document
    used vscode prettier to clean everything up
    created a new branch (after learning that exists)

--- app.js
+++ app.js
@@ -1,23 +1,23 @@
-var intervalId = window.setInterval(function(){
-    for(var i = 0; i < equipmentList.length; i++) {
-        for (var j = 0; j < equipmentList[i].length; j++) {
-            let pieceOfEquipment = equipmentList[i][j]
-            if (typeof(pieceOfEquipment) === 'string') {
-                break
-            }
-            read(pieceOfEquipment)
-        }
+var intervalId = window.setInterval(function () {
+  for (var i = 0; i < equipmentList.length; i++) {
+    for (var j = 0; j < equipmentList[i].length; j++) {
+      let pieceOfEquipment = equipmentList[i][j];
+      if (typeof pieceOfEquipment === "string") {
+        break;
+      }
+      read(pieceOfEquipment);
     }
-    document.getElementById('update').textContent = `Updated ${Date()}`
+  }
+  document.getElementById("update").textContent = `Updated ${Date()}`;
 }, 6000);
 
-let equipmentList = [['avs'],['laus']]
+let equipmentList = [["avs"], ["laus"]];
 
-function read (equipment) {
-    let adminNo = equipment.adminNumber 
-    if (document.getElementById(adminNo)) {
-        equipment.append()
-    } else {
-        equipment.create()
-    }
+function read(equipment) {
+  let adminNo = equipment.adminNumber;
+  if (document.getElementById(adminNo)) {
+    equipment.append();
+  } else {
+    equipment.create();
+  }
 }
