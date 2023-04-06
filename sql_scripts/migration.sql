5995fa0dc12cfdd495651d4d3378426c9d0e21b3
jordan-carlson99 <jordan.carlson.square@gmail.com>
2023-04-06 17:38:06 -0500

    removed equipment_type table, added express server file

--- sql_scripts/migration.sql
+++ sql_scripts/migration.sql
@@ -1,12 +1,7 @@
 DROP TABLE IF EXISTS equipment;
-DROP TABLE IF EXISTS equipment_type;
-CREATE TABLE equipment_type (
-    type_id serial PRIMARY KEY
-);
 CREATE TABLE equipment (
     equipment_id serial PRIMARY KEY,
-    type int NOT NULL,
-    admin_number varchar(255),
-    equipment_status varchar(10),
-    FOREIGN KEY (type) REFERENCES equipment_type(type_id)
+    type varchar(3) NOT NULL,
+    admin_number varchar(255) NOT NULL,
+    equipment_status varchar(10) NOT NULL
 );
---
+++ sql_scripts/seed.sql
@@ -0,0 +1,8 @@
+INSERT INTO equipment (type,admin_number,equipment_status)
+VALUES
+('AV', 1234,'X'),
+('AV', 1235,'X'),
+('AV', 1236,'X'),
+('AV', 1237,'X'),
+('LAU', 4321,'X'),
+('LAU', 4322,'X');
